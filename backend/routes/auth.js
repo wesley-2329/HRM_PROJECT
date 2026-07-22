61273cf83657dcb5ae6ad3da421e32c1766fc131
wesley-2329 <john.290305@gmail.com>
2026-07-22 13:46:19 +0530

    fix: import mongoose at top of auth.js to fix ReferenceError

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const Employee = require('../models/Employee');
